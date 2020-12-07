8fdfbbf5fc39da3328c609ad1ae84b0f7a9c4e48
Phan Văn Tuấn <phanvantuan99hth@gmail.com>
2020-12-07 20:23:55 +0700

    config typecheck

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import { DEVELOPMENT } from 'constant/constant';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT: number | string = process.env.PORT || 5000;
+const PORT: string = process.env.PORT || '5000';
 const MONGODB_URL: string = process.env.MONGODB_URL || 'mongodb://localhost:27017/test';
 const NODE_ENV: string = process.env.NODE_ENV || DEVELOPMENT;
 
--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import App from 'App';
 import { PORT } from 'config/config';
 import todoController from 'todos/todo.controller';
 
-const app = new App([new todoController()], PORT);
+const app = new App([new todoController()], parseInt(PORT));
 app.listen();
